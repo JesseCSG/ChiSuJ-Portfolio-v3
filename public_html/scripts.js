f8d312efc9ca6b1d5eb2bdef43c2f0a6e22a8e46
Jesse <jchisu31@gmail.com>
2015-01-07 09:07:23 -0800

    Portfolio V3-First Update

---
+++ public_html/scripts.js
@@ -0,0 +1,13 @@
+$("document").ready(function() {
+   
+    $(".Klondike-bar").css("background-color", "red");
+    
+    $("#TEXT ~ p").css({"background-color": "blue", "color": "white"});
+    
+    $("p:last").css({"background-color": "green", "color": "purple"});
+    
+    $("div:nth-child(2)").css("background-color", "cyan");
+    
+});
+
+
