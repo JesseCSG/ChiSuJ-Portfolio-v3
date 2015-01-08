ed927e1a944bc6c9fdfc21ddaca7655247d0f1b8
Jesse <jchisu31@gmail.com>
2015-01-08 09:19:17 -0800

    Portfolio V3-Update

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -1,13 +1,29 @@
 $("document").ready(function() {
-   
-    $(".Klondike-bar").css("background-color", "red");
     
-    $("#TEXT ~ p").css({"background-color": "blue", "color": "white"});
+    $(".body").css({"background-image": "url(http://abu0mushlih.files.wordpress.com/2013/12/space-arts-and-astronomical-technology-exploration_238385.jpg)", "size": "700px"});
+   
+    $(".div1").css("background-color", "red");
+    
+    $("#TEXT ~ p").css( "color", "white");
     
     $("p:last").css({"background-color": "green", "color": "purple"});
     
-    $("div:nth-child(2)").css("background-color", "cyan");
+    $("div:nth-child(3)").css("color", "white");
+    
+    $(".div2").css("color", "white");
+    
+    $(".clickToHide").click(function() {
+        $(".clickToHide").hide();
+    });
+    
+    $("#TEXT2").bind("click", alertButtonClick);
+    
+    $("#logo").bind()
     
 });
+
+    function alertButtonClick() {
+        alert("You click, so want you want?");
+    }
 
 
