b783ec5dfbc53877f4f032d5ae1f985f7207660f
Jesse <jchisu31@gmail.com>
2015-01-13 09:21:03 -0800

    Portfolio V3-Update

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -1,37 +1,32 @@
 $("document").ready(function() {
+    
     $(".body").css({"background-image": "url(http://abu0mushlih.files.wordpress.com/2013/12/space-arts-and-astronomical-technology-exploration_238385.jpg)", "size": "700px"});
     
-    $(".div1").css("background-color", "red");
-    
-    $("#TEXT ~ p").css("color", "white");
-    
-    $("p:last").css({"border-style": "solid", "color": "white"});
-    
-    $("div:nth-child(3)").css("color", "white");
-    
-    $(".div2").css("color", "white");
-    
+    $(".div1").css("background-color", "red");    
+    $("#TEXT ~ p").css("color", "white");    
+    $("p:last").css({"border-style": "solid", "color": "white"});    
+    $("div:nth-child(3)").css("color", "white");    
+    $(".div2").css("color", "white");    
     $(".div3").css("color", "white");
     
     $(".clickToHide").click(function() {
         $(".clickToHide").hide();
     });
     
-    $("#TEXT2").bind("click", alertButtonClick);
-    
+    $("#TEXT2").bind("click", alertButtonClick);    
     $("#TEXT2").css({"border-style": "solid", "border-color": "red"});
     
     $(".second").bind("mouseover", mouseOverMe).bind("mouseout", mouseOutMe);
     
-    $("#replaceWHtml").bind("click", replaceWHtml);
-    
+    $("#replaceWHtml").bind("click", replaceWHtml);    
     $("#replaceWText").bind("click", replaceWText);
     
-    $("#addAPara").bind("click", addAPara);
-    
+    $("#addAPara").bind("click", addAPara);    
     $("#removeAPara").bind("click", removeAPara);
-    
-    
+
+    $("#hideLogo").bind("click", hideTheImage);
+    $("#showLogo").bind("click", showTheImage);
+
     
 });
 
@@ -40,26 +35,23 @@ function alertButtonClick() {
 }
 
 function mouseOverMe() {
-    $(".second").html("Get Out O'Here!!");
-}
-
+    $(".second").html("Get Out O'Here!!");}
 function mouseOutMe() {
-    $(".second").html("You Leave!?! You Rude!!");
-}
+    $(".second").html("You Leave!?! You Rude!!");}
 
 function replaceWHtml() {
-    $(".h3Tag").html("<h6>A Random Sentence Appears!</h6>");
-}
-
+    $(".h3Tag").html("<h6>A Random Sentence Appears!</h6>");}
 function replaceWText() {
-    $(".h3Tag").text("A Random Sentence Now Changes!");
-}
+    $(".h3Tag").text("A Random Sentence Now Changes!");}
 
 function addAPara() {
-    $("#randPara").append("<p>Paragragh text and stuff!!</p>");
-}
-
+    $("#randPara").append("<p>Paragragh text and stuff!!</p>");}
 function removeAPara() {
-    $("#randPara").remove();
-}
+    $(".h3Tag").remove();}
 
+function hideTheImage() {
+    $('#image1').hide("clip", {}, 2500);
+}
+       
+function showTheImage() {
+    $("#image1").show("fold", {}, 2500);}
